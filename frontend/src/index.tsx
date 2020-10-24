38d3f570e6335f79f142a45c557b19d7dd8cf40e
pashynskyi <59894423+pashynskyi@users.noreply.github.com>
2020-10-24 15:32:38 +0300

    add frontend folder

---
+++ frontend/src/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core';
+
+import './index.css';
+
+import App from './App';
+import theme from './theme';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <Router>
+          <App />
+      </Router>
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
